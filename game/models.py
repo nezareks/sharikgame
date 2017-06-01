fb134c2d76c7943d4da691c801911a004d503a97
nezareks <nezar08@hotmail.com>
2017-06-01 14:45:54 +0300

    Created all needed models for creating a cup of coffee

--- game/models.py
+++ game/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Bean(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Roast(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Syrup(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Powder(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Coffee(models.Model):
+    name = models.CharField(max_length=255)
+    bean = models.ForeignKey(Bean)
+    roast = models.ForeignKey(Roast)
+    espresso_shots = models.PositiveIntegerField(default=1)
+    water = models.FloatField(default=100)
+    steamed_milk = models.BooleanField(default=False)
+    foam = models.FloatField(default=2)
+    powder = models.ManyToManyField(Powder, blank=True)
+    syrup = models.ManyToManyField(Syrup, blank=True)
+    extra_instructions = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
