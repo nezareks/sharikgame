bd05aa05de70893bf6371567481af7d5c24bb320
nezareks <nezar08@hotmail.com>
2017-06-01 15:44:59 +0300

    completed the edit view for customizing coffee

--- game/urls.py
+++ game/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^home/$', views.home, name='home'),
+    url(r'^edit_coffee/(?P<coffee_id>[0-9]+)/$', views.edit_coffee, name="edit_coffee"),
     url(r'^create_coffee/$', views.create_coffee, name='create_coffee'),
 ]
--- game/views.py
+++ game/views.py
@@ -22,3 +22,24 @@ def create_coffee(request):
         form = CoffeeForm()
         context['form'] = form
         return render(request, 'create_coffee.html', context)
+
+def edit_coffee(request, coffee_id):
+    context = {}
+    coff = Coffee.objects.get(id=coffee_id)
+    context['coffee'] = coff
+
+    if request.method == "POST":
+        form = CoffeeForm(request.POST, instance=coff)
+        context['form'] = form
+        if form.is_valid():
+            coffee = form.save(commit=False)
+            coffee.user = request.user
+            coffee.save()
+            form.save_m2m()
+            return redirect("home")
+        else:
+            return render(request, 'edit_coffee.html', context)
+    else:
+        form = CoffeeForm(instance=coff)
+        context['form'] = form
+        return render(request, 'edit_coffee.html', context)
